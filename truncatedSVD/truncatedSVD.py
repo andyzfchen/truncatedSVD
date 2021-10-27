7cdb390be68e90569d2997c29c235ce2bf6734f0
Andy Chen <chen.andy14@gmail.com>
2021-10-27 01:10:02 -0400

    Added all three datasets to running code.

--- truncatedSVD/truncatedSVD.py
+++ truncatedSVD/truncatedSVD.py
@@ -1,23 +1,29 @@
 import numpy as np
 import EvolvingMatrix as EM
 
-A_full = np.load("../datasets/CISI/CISI.npy")
-m_percent = 0.50
+datasets = [ "CISI", "CRAN", "MED" ]
 
-(m_dim_full, n_dim) = np.shape(A_full)
-m_dim = int(np.ceil(m_dim_full*m_percent))
-s_dim = int(np.floor(m_dim_full*(1-m_percent)))
-k_dim = 50
+for dataset in datasets:
+  print("Performing truncated SVD on dataset "+dataset+".")
+  A_full = np.load("../datasets/"+dataset+"/"+dataset+".npy")
+  m_percent = 0.50
 
-B = A_full[:m_dim,:]
-E = A_full[m_dim:,:]
+  (m_dim_full, n_dim) = np.shape(A_full)
+  m_dim = int(np.ceil(m_dim_full*m_percent))
+  s_dim = int(np.floor(m_dim_full*(1-m_percent)))
+  k_dim = 50
 
-model = EM.EvolvingMatrix(B, k_dim=k_dim)
-model.set_appendix_matrix(E)
+  B = A_full[:m_dim,:]
+  E = A_full[m_dim:,:]
 
-Uk, Sigmak, VHk = model.evolve_matrix(step_dim=s_dim)
+  model = EM.EvolvingMatrix(B, k_dim=k_dim)
+  model.set_appendix_matrix(E)
 
-print("Last singular vector Residual Norm:")
-print(model.get_residual_norm())
-print("Last singular value Relative Error:")
-print(model.get_relative_error())
+  Uk, Sigmak, VHk = model.evolve_matrix(step_dim=s_dim)
+
+  print("Last singular vector Residual Norm:")
+  print(model.get_residual_norm())
+  print("Last singular value Relative Error:")
+  print(model.get_relative_error())
+
+  print()
