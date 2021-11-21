d6cdb6289329634b425edc0acdfbe2b562dc505a
Andy Chen <chen.andy14@gmail.com>
2021-11-21 16:31:44 -0500

    WIP: First implementation of deflated BCG method. A couple points where the algorithm can be improved. Needs to be tested.

--- truncatedSVD/EvolvingMatrix.py
+++ truncatedSVD/EvolvingMatrix.py
@@ -62,6 +62,8 @@ class EvolvingMatrix(object):
   First method of constructing Z = [[U_k 0] [0 I_s]].
   '''
   def evolve_matrix_zha_simon(self, step_dim=None):
+    print("Using Zha Simon method to evolve matrix.")
+
     # default number of appended rows
     if step_dim is None:
       step_dim = self.step_dim
@@ -70,28 +72,31 @@ class EvolvingMatrix(object):
     if step_dim > (self.s_dim - self.n_rows_appended):
       step_dim = self.s_dim - self.n_rows_appended
 
-    Z = np.block(
+    # Z matrix
+    Z_matrix = np.block(
       [[ self.Uk_matrix , np.zeros((self.m_dim+self.n_rows_appended, step_dim)) ],
        [ np.zeros((step_dim, self.k_dim)) , np.eye(step_dim) ]]
     )
 
-    print("Appending ", step_dim, " rows from appendix matrix and evolving truncated matrix.")
-
-    self.A_matrix = np.append(self.A_matrix, self.appendix_matrix[self.n_rows_appended:self.n_rows_appended+step_dim,:], axis=0)
-
+    # kSVD of ZH*A        # TODO: implement unrestarted Lanczos method on ZH*A*AH*Z
+    print("Performing kSVD on ZH*A.")
     (F_matrix, Theta_array, G_matrix) = np.linalg.svd(np.block(
       [[ np.dot(np.diag(self.Sigmak_array), self.VHk_matrix) ],
        [ self.appendix_matrix[self.n_rows_appended:self.n_rows_appended+step_dim,:] ]]
     ), full_matrices=False)
     
+    print("Appending ", step_dim, " rows from appendix matrix and evolving truncated matrix.")
+
+    self.A_matrix = np.append(self.A_matrix, self.appendix_matrix[self.n_rows_appended:self.n_rows_appended+step_dim,:], axis=0)
+
     self.n_rows_appended += step_dim
 
     print("Appended ", self.n_rows_appended, " of ", self.s_dim, " rows from appendix matrix so far.")
     
-
-    self.Uk_matrix = np.dot(Z, F_matrix[:,:self.k_dim])
+    # recalculation of Uk, Sigmak, and VHk
+    print("Recalculating U_k, Sigma_k, and VH_k.")
+    self.Uk_matrix = np.dot(Z_matrix, F_matrix[:,:self.k_dim])
     self.Sigmak_array = Theta_array[:self.k_dim]
-
     self.VHk_matrix = np.dot(np.dot(self.A_matrix.T, self.Uk_matrix), np.diag(1/self.Sigmak_array)).T
 
     print()
@@ -102,8 +107,63 @@ class EvolvingMatrix(object):
   '''
   Second method of constructing Z = [[U_k, X_lambda,r 0] [0 I_s]].
   '''
-  def evolve_matrix_deflated_bcg(self, step_dim=None):
-    return
+  def evolve_matrix_deflated_bcg(self, step_dim=None, r_dim=None):    # TODO: r_dim is varied through [10, 20, 30, 40, 50]
+    print("Using deflated BCG method to evolve matrix.")
+
+    # default number of appended rows
+    if step_dim is None:
+      step_dim = self.step_dim
+
+    # checks if number of appended rows exceeds number of remaining rows in appendix matrix
+    if step_dim > (self.s_dim - self.n_rows_appended):
+      step_dim = self.s_dim - self.n_rows_appended
+
+    # Xlambdar
+    print("Calculating X matrix.")
+    E_matrix = self.appendix_matrix[self.n_rows_appended:self.n_rows_appended+step_dim,:]
+    lambda_value = self.Sigmak_array[0]   # lambda_value should be >= first singular value
+    LHS_matrix = -( np.dot(self.A_matrix, self.A_matrix.T) - lambda_value*np.eye(self.m_dim) )
+    RHS_matrix = np.dot( ( np.eye(self.m_dim) - np.dot(self.Uk_matrix, self.Uk_matrix.T) ), np.dot(self.A_matrix, E_matrix.T) )
+
+    X_matrix = np.zeros((self.m_dim, self.step_dim))      # TODO: currently using CG column by column; need to implement block CG instead
+    for ii in range(step_dim):
+      X_matrix[:,ii] = scipy.sparse.linalg.cg(LHS_matrix, RHS_matrix)
+
+    # rSVD of X
+    print("Performing rSVD on X for X_lambda,r matrix.")
+    (Xlambdar_matrix, Slambdar_array, Ylambdar_matrix) = np.linalg.svd(X_matrix, full_matrices=False)
+
+    # Z matrix
+    Z_matrix = np.block(
+      [[ self.Uk_matrix , Xlambdar_matrix[:,:r_dim], np.zeros((self.m_dim+self.n_rows_appended, step_dim)) ],
+       [ np.zeros((step_dim, self.k_dim+r_dim)) , np.eye(step_dim) ]]
+    )
+
+    # kSVD of ZH*A    # TODO: implement unrestarted Lanczos method on ZH*A*AH*Z
+    print("Performing kSVD on ZH*A.")
+    (F_matrix, Theta_array, G_matrix) = np.linalg.svd(np.block(
+      [[ np.dot(np.diag(self.Sigmak_array), self.VHk_matrix) ],
+       [ Xlambdar_matrix.T, self.A_matrix ],
+       [ self.appendix_matrix[self.n_rows_appended:self.n_rows_appended+step_dim,:] ]]
+    ), full_matrices=False)
+    
+    print("Appending ", step_dim, " rows from appendix matrix and evolving truncated matrix.")
+
+    self.A_matrix = np.append(self.A_matrix, self.appendix_matrix[self.n_rows_appended:self.n_rows_appended+step_dim,:], axis=0)
+
+    self.n_rows_appended += step_dim
+
+    print("Appended ", self.n_rows_appended, " of ", self.s_dim, " rows from appendix matrix so far.")
+    
+    # recalculation of Uk, Sigmak, and VHk
+    print("Recalculating U_k, Sigma_k, and VH_k.")
+    self.Uk_matrix = np.dot(Z_matrix, F_matrix[:,:self.k_dim])
+    self.Sigmak_array = Theta_array[:self.k_dim]
+    self.VHk_matrix = np.dot(np.dot(self.A_matrix.T, self.Uk_matrix), np.diag(1/self.Sigmak_array)).T
+
+    print()
+
+    return self.Uk_matrix, self.Sigmak_array, self.VHk_matrix
 
 
   '''
