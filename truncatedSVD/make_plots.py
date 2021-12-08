d4e7bf6ffa48e6b18f019b91aa79201476d7d54b
Andy Chen <chen.andy14@gmail.com>
2021-12-07 20:06:45 -0500

    Fixed plotting cosmetics.

--- truncatedSVD/make_plots.py
+++ truncatedSVD/make_plots.py
@@ -2,19 +2,23 @@ import numpy as np
 from plotting_helper import plot_relative_errs, plot_residual_norms, plot_stacked_relative_errs, plot_stacked_residual_norms
 import os
 
+'''
 datasets = [ "CISI", "CRAN", "MED", "ML1M" ]
-batch_splits = [ 1, 10, 12 ]
-phis = [ [ 1 ], [ 1, 5, 10 ], [ 1, 6, 12 ] ]
-r_value = 50
+#datasets = [ "CISI" ]
+batch_splits = [ 10 ]
+phis = [ [ 1, 5, 10 ] ]
+r_value = 10
 
 '''
 # debug mode
-datasets = [ "CISI" ]
+#datasets = [ "CISI", "CRAN", "MED", "ML1M" ]
+datasets = [ "Reuters" ]
 batch_splits = [ 1 ]
 phis = [ [ 1 ] ]
-'''
+r_value = 50
 
 evolution_methods = [ "zha-simon", "bcg" ]
+evolution_names = [ "$Z = [U_k, 0; 0, I_s]$", "$Z = [U_k, X_{\lambda,r}; 0, I_s]$" ]
 
 if not os.path.exists("../figures"):
   os.mkdir("../figures")
@@ -40,12 +44,12 @@ for dataset in datasets:
         relative_errors_list.append(relative_errors)
         residual_norms_list.append(residual_norms)
 
-      plot_relative_errs(relative_errors_list, evolution_methods, "relative_errors_"+dataset+"_batch_split_"+str(batch_split), title="Relative Errors for "+dataset)
-      plot_residual_norms(residual_norms_list, evolution_methods, "residual_norms_"+dataset+"_batch_split_"+str(batch_split), title="Residual Norms for "+dataset)
+      plot_relative_errs(relative_errors_list, evolution_names, "relative_errors_"+dataset+"_batch_split_"+str(batch_split), title=dataset)
+      plot_residual_norms(residual_norms_list, evolution_names, "residual_norms_"+dataset+"_batch_split_"+str(batch_split), title=dataset)
 
     # multi-batch update plots
     else:
-      for method in evolution_methods:
+      for ii, method in enumerate(evolution_methods):
         relative_errors_list = []
         residual_norms_list = []
 
@@ -61,5 +65,7 @@ for dataset in datasets:
           relative_errors_list.append(relative_errors)
           residual_norms_list.append(residual_norms)
 
-        plot_stacked_relative_errs(relative_errors_list, phi_list, "relative_errors_"+dataset+"_batch_split_"+str(batch_split)+"_"+method, "Relative Errors for "+dataset+" using "+method)
-        plot_stacked_residual_norms(residual_norms_list, phi_list, "residual_norms_"+dataset+"_batch_split_"+str(batch_split)+"_"+method, "Residual Norms for "+dataset+" using "+method)
+        #plot_stacked_relative_errs(relative_errors_list, phi_list, "relative_errors_"+dataset+"_batch_split_"+str(batch_split)+"_"+method, "Relative Errors for "+dataset+" using "+evolution_names[ii])
+        #plot_stacked_residual_norms(residual_norms_list, phi_list, "residual_norms_"+dataset+"_batch_split_"+str(batch_split)+"_"+method, "Residual Norms for "+dataset+" using "+evolution_names[ii])
+        plot_stacked_relative_errs(relative_errors_list, phi_list, "relative_errors_"+dataset+"_batch_split_"+str(batch_split)+"_"+method, dataset+", "+evolution_names[ii])
+        plot_stacked_residual_norms(residual_norms_list, phi_list, "residual_norms_"+dataset+"_batch_split_"+str(batch_split)+"_"+method, dataset+", "+evolution_names[ii])
