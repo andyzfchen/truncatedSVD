d4ff8c8bda737454e708b3d7d36d8df1502403d8
Shion <smatsumoto97@gmail.com>
2022-01-13 05:51:30 -0500

    Remove old bcg in EM class

--- truncatedSVD/EvolvingMatrix.py
+++ truncatedSVD/EvolvingMatrix.py
@@ -61,9 +61,7 @@ class EvolvingMatrix(object):
         # Initial matrix
         self.initial_matrix = initial_matrix.toarray()  # ensure data is in dense format
         (self.m_dim, self.n_dim) = np.shape(self.initial_matrix)
-        print(
-            f"Initial matrix of evolving matrix set to shape of ( {self.m_dim}, {self.n_dim} )."
-        )
+        print(f"Initial matrix of evolving matrix set to shape of ( {self.m_dim}, {self.n_dim} ).")
 
         # Update matrix after each batch
         self.A = self.initial_matrix
@@ -73,25 +71,20 @@ class EvolvingMatrix(object):
             self.initial_matrix, full_matrices=False
         )
 
-        # Truncated SVD of current update - initialized to rank-k SVD of the initial matrix
+        # Set desired rank of truncated SVD
         if k_dim is None:
             self.k_dim = min(self.m_dim, self.n_dim)
         else:
             self.k_dim = k_dim
 
+        # Get intial truncated SVD
         self.Uk = self.U_true[:, : self.k_dim]
         self.sigmak = self.sigma_true[: self.k_dim]
         self.VHk = self.VH_true[: self.k_dim, :]
 
-        print(
-            f"Initial Uk  matrix of evolving matrix set to shape of ( {np.shape(self.Uk)} )."
-        )
-        print(
-            f"Initial Sigmak matrix of evolving matrix set to shape of ( {np.shape(self.sigmak)} )."
-        )
-        print(
-            f"Initial VHk matrix of evolving matrix set to shape of ( {np.shape(self.VHk)} )."
-        )
+        print(f"Initial Uk  matrix of evolving matrix set to shape of ( {np.shape(self.Uk)} ).")
+        print(f"Initial Sigmak matrix of evolving matrix set to shape of ( {np.shape(self.sigmak)} ).")
+        print(f"Initial VHk matrix of evolving matrix set to shape of ( {np.shape(self.VHk)} ).")
 
         # Initialize matrix to be appended
         self.n_batches = n_batches
@@ -197,145 +190,7 @@ class EvolvingMatrix(object):
             self.A, self.Uk, self.sigmak, self.VHk, self.update_matrix
         )
         return self.Uk, self.sigmak, self.VHk
-
-    def evolve_matrix_deflated_bcg(self, step_dim=None, r_dim=None):
-        """Construct Z using enhanced projection method.
-
-        Z = [[U_k, X_lambda,r 0] [0 I_s]]
-        """
-        print("Using deflated BCG method to evolve matrix.")
-
-        # default number of appended rows
-        if step_dim is None:
-            step_dim = self.step_dim
-
-        # default r dimension
-        if r_dim is None:
-            r_dim = 10
-
-        # checks if number of appended rows exceeds number of remaining rows in appendix matrix
-        if step_dim > (self.s_dim - self.n_rows_appended):
-            step_dim = self.s_dim - self.n_rows_appended
-
-        # checks if Xlambdar has full r rank for Z matrix construction
-        if r_dim > step_dim:
-            r_dim = step_dim
-
-        # Xlambdar
-        print("Calculating X matrix.")
-        E_matrix = self.append_matrix[
-            self.n_rows_appended : self.n_rows_appended + step_dim, :
-        ]
-        svd = sklearn.decomposition.TruncatedSVD(n_components=5, n_iter=20)
-        svd.fit(
-            np.append(
-                self.A_matrix,
-                self.append_matrix[
-                    self.n_rows_appended : self.n_rows_appended + step_dim, :
-                ],
-                axis=0,
-            )
-        )
-        # lambda_value = 1.01 * self.Sigmak_array[0]**2   # lambda_value should be >= first singular value
-        lambda_value = (
-            1.01 * svd.singular_values_[0] ** 2
-        )  # lambda_value should be >= first singular value
-        LHS_matrix = -(
-            np.dot(self.A_matrix, self.A_matrix.T)
-            - lambda_value * np.eye(self.m_dim + self.n_rows_appended)
-        )
-        RHS_matrix = np.dot(
-            (
-                np.eye(self.m_dim + self.n_rows_appended)
-                - np.dot(self.Uk_matrix, self.Uk_matrix.T)
-            ),
-            np.dot(self.A_matrix, E_matrix.T),
-        )
-
-        """
-    X_matrix = np.zeros((self.m_dim+self.n_rows_appended, step_dim))
-    # TODO: currently using CG column by column; need to implement block CG instead
-    for ii in range(step_dim):
-      if (ii+1)%50 == 0:
-        print("Step "+str(ii+1)+" of "+str(step_dim)+".")
-      X_matrix[:,ii] = scipy.sparse.linalg.cg(LHS_matrix, RHS_matrix[:,ii])[0]
-    print("Inverting matrix for X matrix.")
-    """
-        X_matrix = np.dot(np.linalg.inv(LHS_matrix), RHS_matrix)
-
-        # rSVD of X
-        print("Performing randomized rSVD on X for X_lambda,r matrix.")
-        X_matrix = np.dot(X_matrix, np.random.normal(size=(step_dim, 2 * r_dim)))
-        (Xlambdar_matrix, Slambdar_array, Ylambdar_matrix) = np.linalg.svd(
-            X_matrix, full_matrices=False
-        )
-
-        # Z matrix
-        Z_matrix = np.block(
-            [
-                [
-                    self.Uk_matrix,
-                    Xlambdar_matrix[:, :r_dim],
-                    np.zeros((self.m_dim + self.n_rows_appended, step_dim)),
-                ],
-                [np.zeros((step_dim, self.k_dim + r_dim)), np.eye(step_dim)],
-            ]
-        )
-
-        # kSVD of ZH*A    # TODO: implement unrestarted Lanczos method on ZH*A*AH*Z
-        print("Performing kSVD on ZH*A.")
-        ZHA = np.block(
-            [
-                [np.dot(np.diag(self.Sigmak_array), self.VHk_matrix)],
-                [np.dot(Xlambdar_matrix[:, :r_dim].T, self.A_matrix)],
-                [
-                    self.append_matrix[
-                        self.n_rows_appended : self.n_rows_appended + step_dim, :
-                    ]
-                ],
-            ]
-        )
-        """
-    ZHA = np.block([ np.dot(self.VHk_matrix.T, np.diag(self.Sigmak_array)), np.dot(self.A_matrix.T, Xlambdar_matrix[:,:r_dim]), self.appendix_matrix[self.n_rows_appended:self.n_rows_appended+step_dim,:].T ]).T
-    """
-        (F_matrix, Theta_array, G_matrix) = np.linalg.svd(ZHA, full_matrices=False)
-
-        print(
-            "Appending ",
-            step_dim,
-            " rows from appendix matrix and evolving truncated matrix.",
-        )
-
-        self.A_matrix = np.append(
-            self.A_matrix,
-            self.append_matrix[
-                self.n_rows_appended : self.n_rows_appended + step_dim, :
-            ],
-            axis=0,
-        )
-
-        self.n_rows_appended += step_dim
-
-        print(
-            "Appended ",
-            self.n_rows_appended,
-            " of ",
-            self.s_dim,
-            " rows from appendix matrix so far.",
-        )
-
-        # recalculation of Uk, Sigmak, and VHk
-        print("Recalculating U_k, Sigma_k, and VH_k.")
-        self.Uk_matrix = np.dot(Z_matrix, F_matrix[:, : self.k_dim])
-        self.Sigmak_array = Theta_array[: self.k_dim]
-        self.VHk_matrix = np.dot(
-            np.dot(self.A_matrix.T, self.Uk_matrix), np.diag(1 / self.Sigmak_array)
-        ).T
-
-        print()
-
-        return self.Uk_matrix, self.Sigmak_array, self.VHk_matrix
-
+ 
     def calculate_true_svd(self, evolution_method, dataset):
         """Calculate true SVD of the current A matrix."""
         print("Calculating current A matrix of size ", np.shape(self.A))
