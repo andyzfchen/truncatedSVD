9dd578eec7a3d9d25c95d98a0e689e97d8c942d5
rsvarma <emailrsv@gmail.com>
2022-01-25 18:12:04 -0500

    plotting for JSON automation

--- truncatedSVD/run_tests.py
+++ truncatedSVD/run_tests.py
@@ -4,9 +4,10 @@ from os.path import normpath, exists,join
 import numpy as np
 import EvolvingMatrix as EM
 import pdb
+from test_plotter import plot_stacked_residual_norms,plot_stacked_relative_errors
+import sys
 
-
-def perform_updates(dataset,n_batches,phi,model,method,update_method,r_str,save_dir,**kwargs):
+def perform_updates(dataset,n_batches,phi,model,method,update_method,r_str,save_dir,res_norms_list,rel_errs_list,make_plots=False,**kwargs):
        
         for ii in range(n_batches):
             print(f"Batch {str(ii+1)}/{str(n_batches)}.")
@@ -19,24 +20,37 @@ def perform_updates(dataset,n_batches,phi,model,method,update_method,r_str,save_
                 update_method(**kwargs)
 
             # Save results if batch number specified
-            if model.phi in phi:
+            if model.phi in phi or ii == n_batches-1:
                 # Calculate true SVD for this batch
                 model.calculate_true_svd(method, dataset)
 
                 # Caluclate metrics
                 model.save_metrics(save_dir, print_metrics=True, r_str=r_str)
 
+                #gather results for plotting
+                if make_plots:
+                    rel_err = model.get_relative_error(sv_idx=None)
+                    res_norm = model.get_residual_norm(sv_idx=None)  
+
+                    res_norms_list.append(res_norm)
+                    rel_errs_list.append(rel_err)
+
+
+
             print()
 
 
-
-f = open('tests.json')
+if len(sys.argv) != 2:
+    print("Usage: python run_tests.py <path to test json file>")
+    exit()
+f = open(sys.argv[1])
 test_spec = json.load(f)
 
 if not exists(normpath("../cache")):
     mkdir(normpath("../cache"))
 
 for test in test_spec['tests']:
+
     method = test['method']
 
     #Create directory to store results for method
@@ -45,6 +59,8 @@ for test in test_spec['tests']:
 
     for dataset in test['datasets']:
 
+    
+
         #Create directory to store results for method and dataset
         if not exists(normpath(f"../cache/{method}/{dataset}")):
             mkdir(normpath(f"../cache/{method}/{dataset}"))
@@ -52,6 +68,9 @@ for test in test_spec['tests']:
         data = np.load(test_spec['dataset_info'][dataset])
 
         for n_batches in test['n_batches']:
+            batch_phis = [x for x in test['phis_to_plot'] if x <= n_batches]
+            if n_batches not in batch_phis:
+                batch_phis.append(n_batches)
 
             #Calculate data split indices
             m_percent = test['m_percent']
@@ -79,16 +98,21 @@ for test in test_spec['tests']:
                 
 
 
-                phis = set(test['phis_to_plot'])
+                res_norms_list = []
+                rel_errs_list = []    
                 if method =="zha-simon":
                 # Initialize EM object with initial matrix and matrix to be appended and set desired rank of truncated SVD
                     model = EM.EvolvingMatrix(B, n_batches=n_batches, k_dim=k)
                     model.set_append_matrix(E)
                     print()                     
-                    perform_updates(dataset,n_batches,phis,model,method,model.update_svd_zha_simon,"",save_dir)
+                    perform_updates(dataset,n_batches,batch_phis,model,method,model.update_svd_zha_simon,"",save_dir,res_norms_list,rel_errs_list,make_plots=test['make_plots'])
+                    plot_stacked_relative_errors(rel_errs_list,batch_phis,save_dir)
+                    plot_stacked_residual_norms(res_norms_list,batch_phis,save_dir)
                 elif method == "bcg":
                     for r in test['r_values']:
                         for run_num in range(test['num_runs']):
+                            res_norms_list = []
+                            rel_errs_list = []                                
                             save_dir_run = normpath(join(save_dir,f"run_{str(run_num)}"))
                             if not exists(save_dir_run):
                                 mkdir(save_dir_run)
@@ -96,11 +120,15 @@ for test in test_spec['tests']:
                             model.set_append_matrix(E)
                             print()                                 
                             r_str = f"_rval_{str(r)}"
-                            perform_updates(dataset,n_batches,phis,model,method,model.update_svd_bcg,r_str,save_dir_run,lam_coeff=test['lam_coeff'],r=r)
+                            perform_updates(dataset,n_batches,batch_phis,model,method,model.update_svd_bcg,r_str,save_dir_run,res_norms_list,rel_errs_list,make_plots=test['make_plots'],lam_coeff=test['lam_coeff'],r=r)
+                            plot_stacked_relative_errors(rel_errs_list,batch_phis,save_dir_run)
+                            plot_stacked_residual_norms(res_norms_list,batch_phis,save_dir_run)                            
                 else:
                     raise ValueError(
                         f"Error: Update method {method} does not exist. Must be one of the following."                  
                     )
+
+                
 
 
 
--- truncatedSVD/test_plotter.py
+++ truncatedSVD/test_plotter.py
@@ -3,56 +3,6 @@ import matplotlib.pyplot as plt
 from os.path import join,normpath
 
 
-def plot_residual_norms(errs_list, evolution_methods,save_dir, title="Residual Norms for Singular Vectors", ylim=None):
-    position = np.arange(1, errs_list[0].shape[0] + 1)
-
-    fig, ax = plt.subplots(figsize=(4, 3))
-    ax.grid(True, which="both", linewidth=1, linestyle="--", color="k", alpha=0.1)
-    ax.tick_params(which="both", direction="in", bottom=True, top=True, left=True, right=True)
-    for errs, method in zip(errs_list, evolution_methods):
-        ax.plot(position, errs, label=method)
-
-    ax.set_title(title)
-    ax.set_xlabel("Index")
-    ax.set_xlim(position[0], position[-1])
-    ax.set_ylabel("Residual Norm")
-    ax.set_yscale("log")
-
-    # if ylim is None:
-    #     ax.set_ylim(np.asarray(errs_list).min(), np.asarray(errs_list).max())
-    # else:
-    #     ax.set_ylim(ylim[0], ylim[1])
-
-    ax.legend(loc="lower right")
-    plt.savefig(normpath(join(save_dir,f"resnorms.png")), bbox_inches="tight", pad_inches=0.2, dpi=200)
-    plt.close()
-
-
-def plot_relative_errors(errs_list, evolution_methods, save_dir, title="Relative Error for Singular Vectors", ylim=None):
-    position = np.arange(1, errs_list[0].shape[0] + 1)
-
-    fig, ax = plt.subplots(figsize=(4, 3))
-    ax.grid(True, which="both", linewidth=1, linestyle="--", color="k", alpha=0.1)
-    ax.tick_params(which="both", direction="in", bottom=True, top=True, left=True, right=True)
-    for errs, method in zip(errs_list, evolution_methods):
-        ax.plot(position, errs, label=method)
-
-    ax.set_title(title)
-    ax.set_xlabel("Index")
-    ax.set_xlim(position[0], position[-1])
-    ax.set_ylabel("Relative Error")
-    ax.set_yscale("log")
-    
-    # if ylim is None:
-    #     ax.set_ylim(np.asarray(errs_list).min(), np.asarray(errs_list).max())
-    # else:
-    #     ax.set_ylim(ylim[0], ylim[1])
-    
-    ax.legend(loc="lower right")
-    plt.savefig(normpath(join(save_dir,f"relerrs.png")), bbox_inches="tight", pad_inches=0.2, dpi=200)
-    plt.close()
-
-
 def plot_stacked_residual_norms(errs_list, phi_list, save_dir, title="Residual Norms for Singular Vectors", ylim=None):
     position = np.arange(1, errs_list[0].shape[0] + 1)
 
