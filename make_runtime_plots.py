e5f385cb0877ec70eae89f286438d647920234ec
Rohan Varma <emailrsv@gmail.com>
2022-02-03 16:24:42 -0800

    added runtime plotting vs rank, added point markers to plotting tools

--- make_runtime_plots.py
+++ make_runtime_plots.py
@@ -36,15 +36,13 @@ def make_plots(specs_json,cache_dir):
         print(f'Generating for dataset: {dataset}')
         for num_updates in spec['num_updates']:
             print(f'\tGenerating for batch split: {num_updates}')
-            cov_fig,cov_ax = create_figure(f'Covariance Error For {num_updates} Updates','Rank','Covariance Error')
-            proj_fig,proj_ax = create_figure(f'Projection Error For {num_updates} Updates','Rank','Projection Error')
+            fig,ax = create_figure(f'Runtimes For {num_updates} Updates','Rank','Runtime (ms)')
             for method in listdir(normpath(join(cache_dir,dataset))):
                 #pdb.set_trace()
                 if method not in spec['method_label'].keys():
                     continue
                 print(f'\t\tGenerating for method: {spec["method_label"][method]}')
-                cov_errs_dict = {}
-                proj_errs_dict = {}
+                runtime_dict = {}
 
                 for dir in listdir(normpath(join(cache_dir,dataset,method))):
 
@@ -52,62 +50,54 @@ def make_plots(specs_json,cache_dir):
                         k_dim = int(dir.split("_")[-1])
 
                         if method != "bcg":
-                            with open(normpath(join(cache_dir,dataset,method,dir,"covarience_error.pkl")),'rb') as f:
-                                covariance_errs = pickle.load(f) 
+                            max_seen_runtime_phi = 0        
+                            max_file = ""                    
+                            for file in listdir(normpath(join(cache_dir,dataset,method,dir))):
+                                if "runtime" in file:
+                                    phi_num = int(file.split("_")[2].split('.')[0])
+                                    if phi_num > max_seen_runtime_phi:
+                                        max_seen_runtime_phi = phi_num
+                                        max_file = file
 
-                            with open(normpath(join(cache_dir,dataset,method,dir,"projection_error.pkl")),'rb') as f:
-                                proj_errs = pickle.load(f)
+                            with open(normpath(join(cache_dir,dataset,method,dir,max_file)),'rb') as f:
+                                runtime = float(np.load(f)) 
 
-                            cov_errs_dict[k_dim] = covariance_errs[next(reversed(covariance_errs))]
-                            proj_errs_dict[k_dim] = proj_errs[next(reversed(proj_errs))]
+                            runtime_dict[k_dim] = runtime
                         else:
-                            cov_err_sum = 0
-                            proj_err_sum = 0
+                            runtime_sum = 0
                             num_runs = 0
                             for run_dir in listdir(normpath(join(cache_dir,dataset,method,dir))):
-                                with open(normpath(join(cache_dir,dataset,method,dir,run_dir,"covarience_error.pkl")),'rb') as f:
-                                    covariance_errs = pickle.load(f) 
+                                max_seen_runtime_phi = 0        
+                                max_file = ""                    
+                                for file in listdir(normpath(join(cache_dir,dataset,method,dir,run_dir))):
+                                    if "runtime" in file:
+                                        phi_num = int(file.split("_")[2].split('.')[0])
+                                        if phi_num > max_seen_runtime_phi:
+                                            max_seen_runtime_phi = phi_num
+                                            max_file = file
 
-                                with open(normpath(join(cache_dir,dataset,method,dir,run_dir,"projection_error.pkl")),'rb') as f:
-                                    proj_errs = pickle.load(f)
-
-                                cov_err_sum += covariance_errs[next(reversed(covariance_errs))]
-                                proj_err_sum += proj_errs[next(reversed(proj_errs))]
+                                with open(normpath(join(cache_dir,dataset,method,dir,run_dir,max_file)),'rb') as f:
+                                    runtime_sum += float(np.load(f)) 
                                 num_runs += 1
 
-                            cov_errs_dict[k_dim] = cov_err_sum/num_runs
-                            proj_errs_dict[k_dim] = proj_err_sum/num_runs                              
+                            runtime_dict[k_dim] = runtime_sum/num_runs                            
 
-                sorted_cov = sorted(cov_errs_dict.items())
-                cov_ranks = [x[0] for x in sorted_cov]
-                cov_vals = [x[1] for x in sorted_cov]
-                cov_ax.plot(cov_ranks,cov_vals,label=spec['method_label'][method],marker='o')
+                sorted_runtimes = sorted(runtime_dict.items())
+                runtime_ranks = [x[0] for x in sorted_runtimes]
+                runtime_vals = [x[1] for x in sorted_runtimes]
+                ax.plot(runtime_ranks,runtime_vals,label=spec['method_label'][method],marker='o')
 
-                sorted_proj = sorted(proj_errs_dict.items())
-                proj_ranks = [x[0] for x in sorted_proj]
-                proj_vals = [x[1] for x in sorted_proj]
-                proj_ax.plot(proj_ranks,proj_vals,label=spec['method_label'][method],marker='o')
 
-            cov_ax.legend(loc="upper right")
-            proj_ax.legend(loc="upper right")
+            ax.legend(loc="upper right")
 
-            plt.figure(cov_fig.number)
+            plt.figure(fig.number)
             plt.savefig(
-                normpath(join(cache_dir, dataset,f"covariance_errors_batch_split_{num_updates}")),
+                normpath(join(cache_dir, dataset,f"runtimes_batch_split_{num_updates}")),
                 bbox_inches="tight",
                 pad_inches=0.2,
                 dpi=200,
             )
-            plt.close()
-
-            plt.figure(proj_fig.number)
-            plt.savefig(
-                normpath(join(cache_dir, dataset,f"projection_errors_batch_split_{num_updates}")),
-                bbox_inches="tight",
-                pad_inches=0.2,
-                dpi=200,
-            )
-            plt.close()            
+            plt.close()        
 
 
 
@@ -124,6 +114,7 @@ if __name__ == "__main__":
     from os.path import normpath,join
     import pickle
     import matplotlib.pyplot as plt
+    import numpy as np
     import logging
     logging.getLogger().setLevel(logging.CRITICAL)    
 
